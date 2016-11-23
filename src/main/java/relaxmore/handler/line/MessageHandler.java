bf8572b1832a04143a745e02b5a9d484e09835be
Yusuke.Tsuchiya <yusuke.tsuchiya@linecorp.com>
2016-11-23 18:51:11 +0900

    test

--- src/main/java/relaxmore/handler/line/MessageHandler.java
+++ src/main/java/relaxmore/handler/line/MessageHandler.java
@@ -1,11 +1,8 @@
 package relaxmore.handler.line;
 
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-
 /**
  * Created by tsuchiyayuusuke on 2016/11/23.
  */
-@LineMessageHandler
 public class MessageHandler {
 
     public MessageHandler() {
